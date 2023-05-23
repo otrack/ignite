1af30b8004c6abb55c31bf3f36c1c9e01938076d
Boubacar Kane <kaneb@bran>
2023-05-23 22:24:46 +0200

    update

---
+++ analyse_YCSB.py
@@ -0,0 +1,71 @@
+import sys
+import os
+
+YCSB_file_name = sys.argv[1]
+flag_avg = sys.argv[2]
+nb_client = sys.argv[3]
+list_args = sys.argv[4:len(sys.argv)]
+
+
+
+if flag_avg == "True":
+
+    list_nb_client = nb_client.split(" ")
+
+    for nb in list_nb_client:
+
+        for arg in list_args:
+            arg0 = arg.split("-")[0]
+            arg1 = arg.split("-")[1]
+            file_name = "YCSB_"+arg0+"_"+arg1+"_"+nb+"_clients.txt"
+            file_name_avg = "YCSB_"+arg0+"_"+arg1+".txt"
+
+            file = open(file_name, "r")
+
+            if nb == "1":
+                file_avg = open(file_name_avg, "w")
+            else:
+                file_avg = open(file_name_avg, "a")
+
+            sum = 0
+            nb_test = 0
+
+            for line in file.readlines():
+                val = float(line.split(" ")[2])
+                sum += val
+                nb_test += 1
+
+            file_avg.write(nb + " " + str(sum/nb_test) + "\n")
+
+            file.close()
+            file_avg.close()
+
+            os.remove(file_name)
+            
+
+    
+else:
+
+    file_read = open(YCSB_file_name, "r")
+
+    for line in file_read.readlines():
+        
+        for arg in list_args:
+
+            arg0 = arg.split("-")[0]
+            arg1 = arg.split("-")[1]
+            file_name = "YCSB_"+arg0+"_"+arg1+"_"+nb_client+"_clients.txt"
+
+            if arg0 in line and arg1 in line:
+
+                split_line = line.split(",")
+                
+                file_write = open(file_name,"a")
+                
+                # print(nb_client+" "+split_line[len(split_line) - 1])
+                file_write.write(nb_client+" "+split_line[len(split_line) - 1])
+
+                file_write.close()
+                
+    file_read.close()
+
---
+++ btrace/load_FlameGraph.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# kill all the children of the current process
+trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+nbTest=""
+clients="1 2 4 8 16 32 48"
+list_stat="OVERALL-RunTime OVERALL-Throughput"
+hosts="127.0.0.1"
+workload="workloada"
+operationcount=100000
+recordcount=100000
+output_file_load="outputload.txt"
+output_file_run="outputrun.txt"
+
+cd $YCSB_HOME
+
+$YCSB_HOME/bin/ycsb load ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_load
+$YCSB_HOME/bin/ycsb run ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_run &
+
+cd $ASYNC_PROFILER_HOME
+
+./build/bin/asprof -f $YCSB_HOME/flameGraph_YCSB.html $!
+
+cd $IGNITE_HOME/btrace

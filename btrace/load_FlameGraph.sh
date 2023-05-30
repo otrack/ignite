f39482c611c9ba102dc42f0c01e187b022d70c5c
Boubacar Kane <kaneb@bran>
2023-05-30 13:08:16 +0200

    update

--- btrace/load_FlameGraph.sh
+++ btrace/load_FlameGraph.sh
@@ -9,7 +9,7 @@ clients="1 2 4 8 16 32 48"
 list_stat="OVERALL-RunTime OVERALL-Throughput"
 hosts="127.0.0.1"
 workload="workloada"
-operationcount=100000
+operationcount=10000000
 recordcount=100000
 output_file_load="outputload.txt"
 output_file_run="outputrun.txt"
@@ -17,8 +17,8 @@ output_file_run="outputrun.txt"
 nb_client="48"
 cd $YCSB_HOME
 
-python2 $YCSB_HOME/bin/ycsb load ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_load
-python2 $YCSB_HOME/bin/ycsb run ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_run &
+#python2 $YCSB_HOME/bin/ycsb load ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_load
+python2 $YCSB_HOME/bin/ycsb run ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_run 
 
 cd $ASYNC_PROFILER_HOME
 

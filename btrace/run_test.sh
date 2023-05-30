ea58078ba2c1a12638f1ccdaf20c416a0ae76a6a
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-05-30 11:16:10 +0200

    update

--- btrace/run_test.sh
+++ btrace/run_test.sh
@@ -4,34 +4,31 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=""
+nbTest=3
 clients="1 2 4 8 16 32 48"
 list_stat="OVERALL-RunTime OVERALL-Throughput"
 hosts="127.0.0.1"
 workload="workloada"
-operationcount=10000
-recordcount=10000
+operationcount=1000000
+recordcount=1000000
 output_file_load="outputload.txt"
 output_file_run="outputrun.txt"
 
 cd $YCSB_HOME
 
-for test in {1. .3}
+
+for (( c=1; c<=nbTest; c++ ))
 do
-    for nb_client in 1 2 4 8 16 32 48
-    do
-        # $IGNITE_HOME/bin/**ignite.sh** ignite.xml &
-        # ignite_pid = $!
+  for nb_client in 1 2 4 8 16 32 48
+  do
+    # echo "$YCSB_HOME/bin/ycsb load ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_load"
+    python2 $YCSB_HOME/bin/ycsb load ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_load
+    python3 $IGNITE_HOME/btrace/analyse_YCSB.py $IGNITE_HOME/$output_file_load False load $nb_client $list_stat
 
-        # echo "$YCSB_HOME/bin/ycsb load ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_load"
-        $YCSB_HOME/bin/ycsb load ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_load
-        python3 $IGNITE_HOME/btrace/analyse_YCSB.py $IGNITE_HOME/$output_file_load False load $nb_client $list_stat
-
-        $YCSB_HOME/bin/ycsb run ignite -p hosts=$hosts -s -P workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_run
-        python3 $IGNITE_HOME/btrace/analyse_YCSB.py $IGNITE_HOME/$output_file_run False run $nb_client $list_stat
-    done
+    python2 $YCSB_HOME/bin/ycsb run ignite -p hosts=$hosts -s -P $YCSB_HOME/workloads/$workload -threads $nb_client -p operationcount=$operationcount -p recordcount=$recordcount > $IGNITE_HOME/$output_file_run
+    python3 $IGNITE_HOME/btrace/analyse_YCSB.py $IGNITE_HOME/$output_file_run False run $nb_client $list_stat
+  done
 done
-
 
 
 # Make sure that the nb of client match those computed
